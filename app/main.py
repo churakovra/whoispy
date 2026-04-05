85adb48c0e4c60aab58880c333c9aeb9ddc28b00
churakovra <churakov.main@gmail.com>
2026-04-05 15:57:01 +0300

    feature: root page

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.handlers import routers
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 for router in routers:
     app.include_router(router)
