1607b5e00c49f253573b1732b1cb1b63ed33755f
churakovra <churakov.main@gmail.com>
2026-04-07 00:01:04 +0300

    feature: add debugpy

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
+import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.handlers import routers
+
+if os.getenv("DEBUG") == "1":
+    import debugpy
+    debugpy.listen(("0.0.0.0", 5678))
+    print("Debugpy listening on 0.0.0.0:5678")
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
